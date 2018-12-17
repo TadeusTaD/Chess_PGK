e10bd22bddeb41b7974b39ed9c1bf880ac889f48
StopaAndrzej <37457597+StopaAndrzej@users.noreply.github.com>
2018-12-17 18:24:53 +0100

    StartAnimIdle

---
+++ Assets/Scripts/Board/CardAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardAnimation : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("animParam", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
