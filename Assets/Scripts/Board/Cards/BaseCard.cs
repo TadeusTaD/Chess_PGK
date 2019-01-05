2683938256ab36337d21e0cd6d07b7c342a985dc
StopaAndrzej <37457597+StopaAndrzej@users.noreply.github.com>
2019-01-05 03:48:33 +0100

    new graphics, new animations, graveyard

--- Assets/Scripts/Board/Cards/BaseCard.cs
+++ Assets/Scripts/Board/Cards/BaseCard.cs
@@ -41,29 +41,39 @@ public abstract class BaseCard : MonoBehaviour {
 
     public void Update()
     {
-        if (blur != null && mode!=CardMode.onEnterBlur && mode != CardMode.selectedBlur)
+        //if (blur != null && mode != CardMode.onEnterBlur && mode != CardMode.selectedBlur)
+        //{
+        //    if (manaCost <= manager.GetPlayer().manaPool)
+        //    {
+        //        blur.color = new Color(0f / 255, 255f / 255, 0f / 255, (0.75f + (Mathf.Sin(time) / 4)));
+        //        blur.color = new Color(positiveColor.r, positiveColor.g, positiveColor.b, (0.75f + (Mathf.Sin(time) / 4)));
+        //    }
+        //    else
+        //    {
+        //        blur.color = new Color(255f / 255, 0f / 255, 0f / 255, (0.75f + (Mathf.Sin(time) / 4)));
+        //        blur.color = new Color(negativeColor.r, negativeColor.g, negativeColor.b, (0.75f + (Mathf.Sin(time) / 4)));
+        //    }
+        //}
+        if(blur!= null && mode!= CardMode.onEnterBlur && mode != CardMode.selectedBlur)
         {
-            if (manaCost <= manager.GetPlayer().manaPool)
-            {
-                //blur.color = new Color(0f / 255, 255f / 255, 0f / 255, (0.75f + (Mathf.Sin(time) / 4)));
-                blur.color = new Color(positiveColor.r, positiveColor.g, positiveColor.b, (0.75f + (Mathf.Sin(time) / 4)));
-            }
-            else
-            {
-                //blur.color = new Color(255f / 255, 0f / 255, 0f / 255, (0.75f + (Mathf.Sin(time) / 4)));
-                blur.color = new Color(negativeColor.r, negativeColor.g, negativeColor.b, (0.75f + (Mathf.Sin(time) / 4)));
-            }
-        }
-        else if(blur != null && mode == CardMode.onEnterBlur)
-        {
-                 blur.color = new Color(onMouseColor.r, onMouseColor.g, onMouseColor.b, (0.75f + (Mathf.Sin(time) / 4)));
-        }
-        else
-        {
-                blur.color = new Color(selectedColor.r, selectedColor.g, selectedColor.b, (0.75f + (Mathf.Sin(time) / 4)));
+            blur.enabled = false;
         }
 
-        if (time <= 314f)
+        else if(blur != null && mode == CardMode.onEnterBlur)
+        {
+            blur.enabled = true;
+            blur.color = new Color(positiveColor.r, positiveColor.g, positiveColor.b, (0.75f + (Mathf.Sin(time) / 4)));
+        }
+        else if (blur != null && mode == CardMode.selectedBlur)
+        {
+            blur.color = new Color(selectedColor.r, selectedColor.g, selectedColor.b, (0.75f + (Mathf.Sin(time) / 4)));
+        }
+            //else
+            //{
+            //        blur.color = new Color(selectedColor.r, selectedColor.g, selectedColor.b, (0.75f + (Mathf.Sin(time) / 4)));
+            //}
+
+            if (time <= 314f)
             time += 0.05f;
         else
             time -= 0.05f;
---
+++ Assets/Scripts/Board/GraveyardAnimation.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraveyardAnimation : MonoBehaviour
+{
+
+    public Animator anim;
+    public Light light;
+
+    private bool isClicked;
+    public BoxCollider2D collider1;
+    public BoxCollider2D collider2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        isClicked = false;
+        collider1.enabled = true;
+        collider2.enabled = false;
+    }
+
+    void OnMouseEnter()
+    {
+        light.GetComponent<Light>().intensity=2.4f;
+    }
+
+    void OnMouseExit()
+    {
+        light.GetComponent<Light>().intensity = 0.3f;
+    }
+
+    void OnMouseDown()
+    {
+        if (isClicked)
+        {
+            isClicked = false;
+            collider1.enabled = true;
+            collider2.enabled = false;
+            anim.SetBool("checkGrave", false);
+        }
+
+        else
+        {
+            isClicked = true;
+            collider1.enabled = false;
+            collider2.enabled = true;
+            anim.SetBool("checkGrave", true);
+
+        }
+    }
+}
